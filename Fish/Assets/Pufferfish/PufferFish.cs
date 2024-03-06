8c09e48eafaebfd98ec3b9129ae2ab76c7cbf2d8
AndreiMag2024 <amageary0004@mymail.lausd.net>
2024-03-06 10:09:25 -0800

    Organizing and removing unnecessary

--- Fish/Assets/Pufferfish/PufferFish.cs
+++ Fish/Assets/Pufferfish/PufferFish.cs
@@ -28,7 +28,7 @@ public class PufferFish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player =  GameObject.Find("pufferfish");
+        player =  GameObject.Find("player");
         
     }
 
--- Fish/Assets/Swordfish/Swordfish.cs
+++ Fish/Assets/Swordfish/Swordfish.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : MonoBehaviour
+public class Swordfish : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,5 +15,4 @@ public class testScript : MonoBehaviour
     {
         
     }
-
 }
